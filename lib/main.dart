0a8a2ce96f4acd33b643c8da92367e587d731234
fauziarda <fauziardasaputra@gmail.com>
2020-11-21 23:02:50 +0700

    resolve conflict-codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:ceria/screen_guru/home_guru.dart';
-<<<<<<< HEAD
-import 'package:ceria/screen_parent/home_parent.dart';
-=======
 import 'package:ceria/screen_guru/listkehadiran_guru.dart';
 import 'package:ceria/screen_parent/daftarsiswa_parent.dart';
 import 'package:ceria/screen_parent/home_parent.dart';
 import 'package:ceria/screen_parent/isikehadiran_parent.dart';
->>>>>>> 76a6e45219e207fbc40e13c2463d6386734e26c0
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
@@ -22,11 +18,7 @@ class MyApp extends StatelessWidget {
       // home: ShowAssignment(
       //   kelas: constKelas[1],
       // ),
-<<<<<<< HEAD
-      home: HomeParent(),
-=======
       home: IsiKehadiranParent(),
->>>>>>> 76a6e45219e207fbc40e13c2463d6386734e26c0
       debugShowCheckedModeBanner: false,
     );
   }
