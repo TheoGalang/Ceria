65c6af215de1bdd47756632385411bf69ff7d713
fauziarda <fauziardasaputra@gmail.com>
2020-11-22 16:14:02 +0700

    modifing appicon and splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:ceria/screen_guru/home_guru.dart';
-import 'package:ceria/screen_guru/listkehadiran_guru.dart';
-import 'package:ceria/screen_parent/daftarsiswa_parent.dart';
 import 'package:ceria/screen_parent/home_parent.dart';
-import 'package:ceria/screen_parent/isikehadiran_parent.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
@@ -12,13 +9,29 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
+  final color = MaterialColor(
+    0xFF41348C,
+    const <int, Color>{
+      50: const Color(0xFF41348C),
+      100: const Color(0xFF41348C),
+      200: const Color(0xFF41348C),
+      300: const Color(0xFF41348C),
+      400: const Color(0xFF41348C),
+      500: const Color(0xFF41348C),
+      600: const Color(0xFF41348C),
+      700: const Color(0xFF41348C),
+      800: const Color(0xFF41348C),
+      900: const Color(0xFF41348C),
+    },
+  );
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.deepPurple));
     return MaterialApp(
-      // home: ShowAssignment(
-      //   kelas: constKelas[1],
-      // ),
-      home: IsiKehadiranParent(),
+      title: "CERIA",
+      theme: ThemeData(primarySwatch: color),
+      home: HomeParent(),
       debugShowCheckedModeBanner: false,
     );
   }
